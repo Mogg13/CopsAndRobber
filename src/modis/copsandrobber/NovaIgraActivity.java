e9b1c42a05a6f39fdd5a6fe9aaaee655a7e787df
Mladen Gligorijevic <mladen.og@gmail.com>
2013-01-12 00:53:15 +0100

    master

--- src/modis/copsandrobber/NovaIgraActivity.java
+++ src/modis/copsandrobber/NovaIgraActivity.java
@@ -169,7 +169,10 @@ public class NovaIgraActivity extends Activity implements OnItemSelectedListener
     				Intent i;
     				i = new Intent (context, MapaActivity.class);
     				i.putExtra("imeIgre", igra);
-    				i.putExtra("igrac", igrac);
+    				i.putExtra("uloga", igrac.getUloga());
+    				i.putExtra("lat", igrac.getLatitude());
+    				i.putExtra("lon", igrac.getLongitude());
+    				i.putExtra("reg_id", igrac.getReg_id());
         			startActivity(i);
     				Toast.makeText(context, "Kreirana igra: " + ime , Toast.LENGTH_SHORT).show();
     			}
