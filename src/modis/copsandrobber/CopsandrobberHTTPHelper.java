5dcf9675fbdba0a567364607eadd1f9d9f5329d8
Mladen Gligorijevic <mladen.og@gmail.com>
2013-01-24 15:30:42 +0100

    master

--- src/modis/copsandrobber/CopsandrobberHTTPHelper.java
+++ src/modis/copsandrobber/CopsandrobberHTTPHelper.java
@@ -21,7 +21,7 @@ import org.json.JSONObject;
 public class CopsandrobberHTTPHelper {
 	
 	//private static final String url = "http://uhvatilopova.site11.com";
-	private static final String url = "http://copsandrobber.eu5.org/";
+	private static final String url = "http://copsandrobber.freetzi.com/";
 	
 	public static String napraviNovuIgru(Igrac igrac, String imeIgre)
 	{
